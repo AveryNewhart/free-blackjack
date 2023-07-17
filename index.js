fae0accf4cbbc8882f433af6220a031d654fca07
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-16 20:12:43 -0400

    more styling and working on game function

---
+++ index.js
@@ -0,0 +1,14 @@
+(function () {
+
+    var game = new Game();
+
+
+    function Game(){
+        this.newGame = function(){
+            var wager = $.trim($('#wager').val());
+
+            
+        }
+    }
+
+});
